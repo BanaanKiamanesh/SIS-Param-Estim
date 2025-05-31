7370c4862ebcd3d7e8abe44575b580d1b1fa6ac5
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2025-05-31 20:12:57 +0200

    Remove comments and fix plot A

--- net_sis/likelihood.m
+++ net_sis/likelihood.m
@@ -1,11 +1,14 @@
 %% Calculate the Likelihood
+% Calculate the log likelihood for a set of parameters and the observed
+% data.
+% 
 % Assuming a gaussian distribution
 function L = likelihood(theta, tspan, y_obs, y0, sigma)
     A = unflatten(theta(1:end - 1));
     gamma_ = theta(end);
 
     try
-        [t, y_sim] = sim_net_sis(y0, A, gamma_, tspan);
+        [~, y_sim] = sim_net_sis(y0, A, gamma_, tspan);
     catch
         disp('A=')
         disp(A)
@@ -17,6 +20,5 @@ function L = likelihood(theta, tspan, y_obs, y0, sigma)
     residual = y_obs - y_sim;
     n = numel(y_obs);
     
-    %L = (1 / (sqrt(2 * pi) * sigma))^n * exp(-0.5 * sum(residual(:).^2) / sigma^2);
     L = -0.5 * n * log(2 * pi * sigma^2) - sum(residual(:).^2) / (2 * sigma^2);
 end
--- net_sis/plot_X_trajectory.m
+++ net_sis/plot_X_trajectory.m
@@ -3,5 +3,5 @@ function plot_X_trajectory(X)
     figure;
     title('MCMC Trajectory')
     % Restrict to 1st component of A and gamma
-    plot(X(1, :), X(end, :), 'ko--', X(1,1), X(end,1), 'r*')
+    plot(X(1, :), X(end, :), 'o--', X(1,1), X(end,1), 'r*')
 end
