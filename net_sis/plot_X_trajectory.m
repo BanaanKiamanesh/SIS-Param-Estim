39059b8eb276c463573da02356b32fe12ce4e232
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2025-06-01 10:46:27 +0200

    Simplify Plot X Trajectory

--- net_sis/plot_X_trajectory.m
+++ net_sis/plot_X_trajectory.m
@@ -1,20 +1,19 @@
 %% MCMC Trajectory Plot
 function plot_X_trajectory(X)
     figure;
-    hold on;
     title('MCMC Trajectory')
 
+    A_11 = X(1, :)
+    gamma_ = X(end, :)
+
     % Plot Trajectory
+    hold on;
     plot(X(1, :), X(end, :), 'o--', X(1,1), X(end,1), 'r*')
 
-    % Fit 2D gaussians to the data and draw ellipsis highlighting 1, 2 and
+    % Fit a 2D Gaussian to the data and draw ellipsis highlighting 1, 2 and
     % 3 std. deviation.
-    x = X(1, :)';
-    y = X(end, :)';
-    data = [x, y];
-
-    mu = mean(data);
-    Sigma = cov(data);
+    mu = mean([A_11', gamma_']);
+    Sigma = cov([A_11', gamma_']);
 
     plot_gaussian_ellipse(mu, Sigma, 1, 'g');
     plot_gaussian_ellipse(mu, Sigma, 2, 'y');
